5c9921c2497e59be11b179b6c4d72e5a683bb6b3
sbx_user1051 <sbx_user1051@169.254.12.245>
2025-06-03 15:41:36 +0000

    Tue Jun 03 15:41:36 UTC 2025

--- lib/solutions/TST/test_rbt.py
+++ lib/solutions/TST/test_rbt.py
@@ -1,10 +1,24 @@
 import unittest
 
+from solutions.RBT.rabbit_hole_solution import RabbitHoleSolution
+
+three_row_four_column_warren = """\
++---+---+---+---+
+|   |   |   |   |
++---+---+---+---+
+|   |   |   |   |
++---+---+---+---+
+|   |   |   |   |
++---+---+---+---+
+"""
 
 class MyTestRabbitHole(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        warren = RabbitHoleSolution().rabbit_hole(3, 4)
+
+        self.assertEqual(warren, three_row_four_column_warren)
 
 
 if __name__ == '__main__':
     unittest.main()
+
