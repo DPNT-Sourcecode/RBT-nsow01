79053ea80a5ddb3a74ee0be02431151e2261370e
sbx_user1051 <sbx_user1051@169.254.12.245>
2025-06-03 15:56:36 +0000

    Tue Jun 03 15:56:36 UTC 2025

--- lib/solutions/TST/test_rbt.py
+++ lib/solutions/TST/test_rbt.py
@@ -17,22 +17,23 @@ THREE_ROW_FOUR_COLUMN_WARREN = """\
 +---+---+---+---+"""
 
 class MyTestRabbitHole(unittest.TestCase):
-    def test_three_rows_four_columns(self):
-        warren = RabbitHoleSolution().rabbit_hole(3, 4, None, None)
-
-        self.assertEqual(warren, THREE_ROW_FOUR_COLUMN_WARREN)
+    # def test_three_rows_four_columns(self):
+    #     warren = RabbitHoleSolution().rabbit_hole(3, 4, None, None)
+    #
+    #     self.assertEqual(warren, THREE_ROW_FOUR_COLUMN_WARREN)
 
     def test_zero_rows(self):
         warren = RabbitHoleSolution().rabbit_hole(0, 4, None, None)
 
-        self.assertEqual(warren, "")
+        self.assertEqual("", warren)
 
     def test_one_row_one_column(self):
         warren = RabbitHoleSolution().rabbit_hole(1, 1, None, None)
 
-        self.assertEqual(warren, ONE_ROW_ONE_COLUMN_WARREN)
+        self.assertEqual(ONE_ROW_ONE_COLUMN_WARREN, warren)
 
 
 if __name__ == '__main__':
     unittest.main()
+
 
