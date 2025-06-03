7e710c8145ec577377f018d1415702b27c08505f
sbx_user1051 <sbx_user1051@169.254.12.245>
2025-06-03 15:53:36 +0000

    Tue Jun 03 15:53:36 UTC 2025

--- lib/solutions/RBT/rabbit_hole_solution.py
+++ lib/solutions/RBT/rabbit_hole_solution.py
@@ -11,7 +11,11 @@ THREE_ROW_FOUR_COLUMN_WARREN = """\
 class RabbitHoleSolution:
 
     def rabbit_hole(self, rows, columns, digging_moves, rendering_options):
-        line_1 = "+---"
-        return ""
+        if not rows or columns:
+            return ""
+        horizontal_divider = "+---" + "+"
+        vertical_divider = "|   " + "|"
+        return horizontal_divider + "\n" + vertical_divider + "\n" + horizontal_divider
+
 
 
