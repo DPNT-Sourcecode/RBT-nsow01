0a723d6afddde3ad55369da8df71335faf5b8839
sbx_user1051 <sbx_user1051@169.254.12.245>
2025-06-03 16:34:49 +0000

    Tue Jun 03 16:34:49 UTC 2025

--- lib/solutions/RBT/rabbit_hole_solution.py
+++ lib/solutions/RBT/rabbit_hole_solution.py
@@ -17,3 +17,16 @@ class RabbitHoleSolution:
         vertical_divider = "|   " * columns + "|"
         return (horizontal_divider + "\n" + vertical_divider + "\n") * rows + horizontal_divider
 
+class RouteMatrix:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self._list_of_lists = [[0] * columns] * columns
+
+    def __str__(self):
+        pprint.pformat(self.rows)
+
+def dig_route(rows, columns, digging_moves):
+    return
+
+
