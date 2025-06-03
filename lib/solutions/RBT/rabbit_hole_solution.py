602021c91ba0f496d0e7332eca66cba9a7371461
sbx_user1051 <sbx_user1051@169.254.12.245>
2025-06-03 15:59:36 +0000

    Tue Jun 03 15:59:36 UTC 2025

--- lib/solutions/RBT/rabbit_hole_solution.py
+++ lib/solutions/RBT/rabbit_hole_solution.py
@@ -11,11 +11,12 @@ THREE_ROW_FOUR_COLUMN_WARREN = """\
 class RabbitHoleSolution:
 
     def rabbit_hole(self, rows, columns, digging_moves, rendering_options):
-        if not rows or columns:
+        if not rows or not columns:
             return ""
-        horizontal_divider = "+---" + "+"
-        vertical_divider = "|   " + "|"
+        horizontal_divider = "+---" * columns + "+"
+        vertical_divider = "|   " * columns + "|"
         return horizontal_divider + "\n" + vertical_divider + "\n" + horizontal_divider
+
 
 
 
--- lib/solutions/TST/test_rbt.py
+++ lib/solutions/TST/test_rbt.py
@@ -7,6 +7,11 @@ ONE_ROW_ONE_COLUMN_WARREN = """\
 |   |
 +---+"""
 
+ONE_ROW_TWO_COLUMN_WARREN = """\
++---+---+
+|   |   |
++---+---+"""
+
 THREE_ROW_FOUR_COLUMN_WARREN = """\
 +---+---+---+---+
 |   |   |   |   |
@@ -15,6 +20,7 @@ THREE_ROW_FOUR_COLUMN_WARREN = """\
 +---+---+---+---+
 |   |   |   |   |
 +---+---+---+---+"""
+
 
 class MyTestRabbitHole(unittest.TestCase):
     # def test_three_rows_four_columns(self):
@@ -32,8 +38,14 @@ class MyTestRabbitHole(unittest.TestCase):
 
         self.assertEqual(ONE_ROW_ONE_COLUMN_WARREN, warren)
 
+    def test_one_row_two_column(self):
+        warren = RabbitHoleSolution().rabbit_hole(1, 1, None, None)
+
+        self.assertEqual(ONE_ROW_TWO_COLUMN_WARREN, warren)
+
 
 if __name__ == '__main__':
     unittest.main()
+
 
 
