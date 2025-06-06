60dbd4bf12293723b21ed529e8c1e1d7ebcd928e
sbx_user1051 <sbx_user1051@169.254.10.163>
2025-06-06 14:39:50 +0000

    Fri Jun 06 14:39:50 UTC 2025

--- lib/solutions/RBT/rabbit_hole_solution.py
+++ lib/solutions/RBT/rabbit_hole_solution.py
@@ -51,9 +51,9 @@ class RabbitHoleSolution:
                 else:
                     vertical_divider += "|   "
             vertical_divider += "|"
-            warren += horizontal_divider + "\n" + vertical_divider * vertical_scale + "\n"
+            warren += horizontal_divider + "\n" + (vertical_divider + "\n") * vertical_scale
 
-        solid_horizontal_divider = "+---" * columns + "+"
+        solid_horizontal_divider = ("+" + "-" * horizontal_scale) * columns + "+"
         return warren + solid_horizontal_divider
 
 
@@ -143,6 +143,7 @@ def dig_route(rows, columns, digging_moves):
         for direction in digging_moves[1:]:
             route.move(direction)
     return route
+
 
 
 
