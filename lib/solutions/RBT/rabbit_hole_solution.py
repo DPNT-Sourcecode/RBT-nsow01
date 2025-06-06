dc2840de3e36c121260722aa81e31d657f401d35
sbx_user1051 <sbx_user1051@169.254.10.163>
2025-06-06 10:29:05 +0000

    Fri Jun 06 10:29:05 UTC 2025

--- lib/solutions/RBT/rabbit_hole_solution.py
+++ lib/solutions/RBT/rabbit_hole_solution.py
@@ -31,7 +31,7 @@ class RabbitHoleSolution:
             vertical_divider = ""
             for column_index in range(route.columns):
                 if route.cell(row_index, column_index) == EnteredFrom.LEFT or (
-                    column_index > route.columns and route.cell(row_index, column_index + 1) == EnteredFrom.RIGHT
+                    column_index < route.columns - 1 and route.cell(row_index, column_index + 1) == EnteredFrom.RIGHT
                 ):
                     vertical_divider += "    "
                 else:
@@ -101,4 +101,5 @@ def dig_route(rows, columns, digging_moves):
         for direction in digging_moves[1:]:
             route.move(direction)
     return route
+
 
